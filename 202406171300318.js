d06964b1ab2fecb671d195351da7d193b3e30e4a
ZuoZ_NeT <90815686+HuaQiPro@users.noreply.github.com>
2024-06-17 13:00:20 +0800

    Upload by PicGo

---
+++ 202406171300318.js
@@ -0,0 +1,26 @@
+/*-----------------------播放器 end---------------------------*/
+if (typeof huaqi !== "undefined") {
+    // 创建 iframe 元素
+    var iframe = document.createElement("iframe");
+
+    // 设置 iframe 属性
+    iframe.setAttribute("width", "100%");
+    iframe.setAttribute("height", "100%");
+    iframe.setAttribute("src", "https://www.huaqi.live/?url=" + encodeURIComponent(huaqi.dz) + "&id=" + huaqi.id + "&api=" + encodeURIComponent(window.location.origin + huaqi.api) + "&ename=" + encodeURIComponent(huaqi.ename));
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("border", "0");
+    iframe.setAttribute("marginwidth", "0");
+    iframe.setAttribute("marginheight", "0");
+    iframe.setAttribute("scrolling", "no");
+    iframe.setAttribute("allowfullscreen", "true");
+    iframe.setAttribute("mozallowfullscreen", "true");
+    iframe.setAttribute("msallowfullscreen", "true");
+    iframe.setAttribute("oallowfullscreen", "true");
+    iframe.setAttribute("webkitallowfullscreen", "true");
+    iframe.setAttribute("security", "restricted");
+    iframe.setAttribute("sandbox", "allow-same-origin allow-forms allow-scripts allow-popups");
+    iframe.style.border = "none";
+
+    // 将 iframe 添加到页面中的指定位置
+    document.getElementById("player").appendChild(iframe);
+}
